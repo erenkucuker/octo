82d81e898f801395bb2b09a2eef54dcbc668fd42
Your Name <you@example.com>
2021-10-14 23:21:56 +0300

    initialized

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
+use App\Traits\ApiResponds;
+
+class Handler extends ExceptionHandler
+{
+    use ApiResponds;
+    
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Throwable  $exception
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if(app()->environment('production'))
+        {
+            return $this->customApiResponse($exception);
+        }
+        return parent::render($request, $exception);
+    }
+
+    private function customApiResponse($exception)
+    {
+        if (method_exists($exception, 'getStatusCode')) 
+        {
+            $statusCode = $exception->getStatusCode();
+        } else 
+        {
+            $statusCode = 500;
+        }
+
+        $response = [];
+
+        switch ($statusCode) {
+            case 401:
+                $response['message'] = 'Unauthorized';
+                break;
+            case 403:
+                $response['message'] = 'Forbidden';
+                break;
+            case 404:
+                $response['message'] = 'Not Found';
+                break;
+            case 405:
+                $response['message'] = 'Method Not Allowed';
+                break;
+            case 422:
+                $response['message'] = $exception->original['message'];
+                $response['errors'] = $exception->original['errors'];
+                break;
+            default:
+                $response['message'] = ($statusCode == 500) ? 'Whoops, looks like something went wrong' : $exception->getMessage();
+                break;
+        }
+
+        if (config('app.debug')) {
+            $response['trace'] = $exception->getTrace();
+            $response['code'] = $exception->getCode();
+        }
+
+        $response['status'] = $statusCode;
+
+        return $this->failure($response, $statusCode);
+    }
+
+}
---
+++ app/Traits/ApiResponds.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Traits;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+
+trait ApiResponds
+{
+  protected function success($message = null, $data = [], $link = [], $meta = [], $status = 200)
+  {
+
+    if ($data instanceof LengthAwarePaginator) {
+      return response([
+        'success' => true,
+        'data' => $data->items(),
+        'link' => [
+          'next_page_url' => $data->nextPageUrl(),
+          'previous_page_url' => $data->previousPageUrl(),
+        ],
+        'meta' => [
+          'per_page' => $data->perPage(),
+          'current_page' => $data->currentPage(),
+          'total' => $data->total(),
+          'last_page' => $data->lastPage()
+
+        ],
+        'message' => $message,
+      ], $status);
+    }
+
+    return response([
+      'success' => true,
+      'data' => $data,
+      'link' => $link,
+      'meta' => $meta,
+      'message' => $message,
+    ], $status);
+  }
+
+  protected function failure($message, $status = 422, $exceptions = [])
+  {
+    return response([
+      'success' => false,
+      'message' => $message,
+      'errors' => $exceptions,
+    ], $status);
+  }
+}
